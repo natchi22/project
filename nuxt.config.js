1c87ce2f28e8cc49e077daba06f7730769cb7fd4
Kunjanaphorn Boonmak <kunjanaphorn.b@gmail.com>
2021-02-25 18:23:40 +0700

    ทดสอบเปลี่ยนรูป profile

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,5 @@
 export default {
-  // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
-  ssr: false,
-
-  // Global page headers (https://go.nuxtjs.dev/config-head)
+  ssr: true,
   head: {
     title: 'checking',
     meta: [
@@ -15,30 +12,18 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
-
-  // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [
     'ant-design-vue/dist/antd.css',
     '~/style/style.css'
   ],
-
-  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
     '@/plugins/antd-ui'
   ],
-
-  // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
-
-  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
   ],
-
-  // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
   ],
-  
-  // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
   }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -66,51 +66,51 @@ export default {
         	if (liff.isLoggedIn()) {
 				const queryString = decodeURIComponent(window.location.search).replace("?liff.state=", "")
                 const params = new URLSearchParams(queryString)
-          	liff.getProfile().then(async (profile) => { 
-            	const userId = profile.userId ///เอาuser id line จากที่ได้ตรงนี้ไปเก็บใน firestore get เข้าไปพร้อมกับหน้าfreelance index
-            	const image = profile.pictureUrl
-            	const name = profile.displayName
-            	this.userId = userId
-            	this.name = name
-            	this.image = image
-				this.saveProfile(profile)
-				if (params.get('page') === 'freelance'){
-					const getInfo = await this.$fireStore.collection("Freelance").where("lineId",'==',this.profile.userId ).get()
-					getInfo.forEach((doc)=>{
-						this.inforFrelance = doc.data()
-					}) 
-					if(this.inforFrelance.lineId && 
-						this.inforFrelance.firstName && 
-						this.inforFrelance.lastName && 
-						this.inforFrelance.phone)
-					{
-							const freelance = await this.$fireStore.collection("Freelance").where("lineId",'==',this.profile.userId ).get()
-							freelance.forEach((doc)=>{
-							this.freelanceData = doc.data()
-							})
-
-						const dateTime = await this.$fireStore.collection("Task")
-						.where("freelanceId",'==',  this.freelanceData.freelanceId)
-						.where("status",'==',  false).get()
-						dateTime.forEach((doc)=>{
-							this.task = doc.data()
+          		liff.getProfile().then(async (profile) => { 
+					const userId = profile.userId ///เอาuser id line จากที่ได้ตรงนี้ไปเก็บใน firestore get เข้าไปพร้อมกับหน้าfreelance index
+					const image = profile.pictureUrl
+					const name = profile.displayName
+					this.userId = userId
+					this.name = name
+					this.image = image
+					this.saveProfile(profile)
+					if (params.get('page') === 'freelance'){
+						const getInfo = await this.$fireStore.collection("Freelance").where("lineId",'==',this.profile.userId ).get()
+						getInfo.forEach((doc)=>{
+							this.inforFrelance = doc.data()
 						}) 
+						if(this.inforFrelance.lineId && 
+							this.inforFrelance.firstName && 
+							this.inforFrelance.lastName && 
+							this.inforFrelance.phone)
+						{
+								const freelance = await this.$fireStore.collection("Freelance").where("lineId",'==',this.profile.userId ).get()
+								freelance.forEach((doc)=>{
+								this.freelanceData = doc.data()
+								})
 
-						if (this.task !== null) {
-							this.$router.replace('/freelance/checkout')
+							const dateTime = await this.$fireStore.collection("Task")
+							.where("freelanceId",'==',  this.freelanceData.freelanceId)
+							.where("status",'==',  false).get()
+							dateTime.forEach((doc)=>{
+								this.task = doc.data()
+							}) 
+
+							if (this.task !== null) {
+								this.$router.replace('/freelance/checkout')
+							}
+							else {
+								this.$router.replace('/freelance/checkin')
+							}
+							
 						}
-						else {
-							this.$router.replace('/freelance/checkin')
+						else{
+							this.$router.replace('/freelance')
 						}
-						
 					}
-					else{
-						this.$router.replace('/freelance')
+					else if (params.get('page') === 'profile'){
+						this.$router.replace('/employ/project')
 					}
-				}
-				else if (params.get('page') === 'profile'){
-					this.$router.replace('/employ/project')
-				}
 				// else if (params.get('page') === 'history'){
 				// 	this.$router.replace('/freelance/history')
 				// } 
